1e050e5dda84f9463c3d15c4229baf8ffc182a2d
Ayan Upadhaya <ayanu881@gmail.com>
2026-01-13 02:55:26 +0600

    layout updated

--- client/src/components/layout/DashboardLayout.jsx
+++ client/src/components/layout/DashboardLayout.jsx
@@ -8,12 +8,12 @@ export function DashboardLayout() {
   const [showSidebar, setShowSidebar] = useState(true);
 
   return (
-    <div className="bg-white h-screen w-full overflow-hidden">
+    <div className="bg-[#f4f5fe] h-screen w-full overflow-hidden">
       <div className="flex items-start h-full">
         <Sidebar showSidebar={showSidebar}></Sidebar>
         <div className="h-full flex-1 w-full overflow-hidden">
           <Navbar toggleSidebar={() => setShowSidebar((prev) => !prev)} />
-          <div className="h-[calc(100%-20px)] overflow-auto bg-[#f4f5fe]">
+          <div className="h-[calc(100%-20px)] overflow-auto">
             <Outlet />
           </div>
         </div>
